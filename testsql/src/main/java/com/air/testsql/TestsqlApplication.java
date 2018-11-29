f4b066de73f970dc0a32c86f011657db034198fe
gonglt1 <gonglt1@lenovo.com>
2018-11-29 17:29:18 +0800

    first commit

---
+++ testsql/src/main/java/com/air/testsql/TestsqlApplication.java
@@ -0,0 +1,12 @@
+package com.air.testsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsqlApplication.class, args);
+    }
+}
